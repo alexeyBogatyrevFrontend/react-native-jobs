aaf6163a726575581c663ae5c0e8f04c7a02ea71
Bogatyrev Alexey <alexey.bogatyrev.frontend@gmail.com>
2023-09-25 18:52:16 +0600

    add tabs and some info for them in job-details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -15,10 +15,12 @@ import {
 	JobFooter,
 	JobTabs,
 	ScreenHeaderBtn,
-	Specifics,
 } from '../../components'
+import Specifics from '../../components/jobdetails/specifics/Specifics'
 import { COLORS, icons, SIZES } from '../../constants'
 import useFetch from '../../hook/useFetch'
+
+const tabs = ['About', 'Qualifications', 'Responsibilities']
 
 const JobDetails = () => {
 	const params = useSearchParams()
@@ -29,8 +31,28 @@ const JobDetails = () => {
 	})
 
 	const [refreshing, setRefreshing] = useState(false)
+	const [activeTab, setActiveTab] = useState(tabs[0])
 
 	const onRefresh = () => {}
+
+	const diaplayTabContent = () => {
+		switch (activeTab) {
+			case 'Qualifications':
+				return (
+					<Specifics
+						title='Qualifications'
+						points={data[0].job_highlights?.qualifications ?? ['N/A']}
+					/>
+				)
+				break
+			case 'About':
+				break
+			case 'Responsibilities':
+				break
+			default:
+				break
+		}
+	}
 
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -59,7 +81,31 @@ const JobDetails = () => {
 					refreshControl={
 						<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
 					}
-				></ScrollView>
+				>
+					{isLoading ? (
+						<ActivityIndicator size='large' color={COLORS.primary} />
+					) : error ? (
+						<Text>Went something wrong...</Text>
+					) : data.length === 0 ? (
+						<Text>No data</Text>
+					) : (
+						<View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+							<Company
+								companyLogo={data[0].employer_logo}
+								jobTitle={data[0].job_title}
+								companyName={data[0].employer_name}
+								location={data[0].job_country}
+							/>
+							<JobTabs
+								tabs={tabs}
+								activeTab={activeTab}
+								setActiveTab={setActiveTab}
+							/>
+
+							{diaplayTabContent()}
+						</View>
+					)}
+				</ScrollView>
 			</>
 		</SafeAreaView>
 	)
